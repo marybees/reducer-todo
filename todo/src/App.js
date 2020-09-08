b9bb507df424d2bbdba5d85dbbd4f0783bab4f28
Mary Baker <meb601@gmail.com>
2020-09-08 11:30:32 -0400

    Build out App.js with useReducer

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+
+const ACTIONS = {
+  ADD_TODO: 'add-todo'
+}
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false }
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+    setName('')
+  }
+
+  console.log(todos)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
+      </form>
+    </>
   );
 }
 
