7a306fb105c14fc9ec606c0cdac95ea0bb25fc60
Mary Baker <meb601@gmail.com>
2020-09-08 12:02:56 -0400

    Build out Todo component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,32 @@
 import React, { useReducer, useState } from 'react';
+import Todo from './Todo'
 
-const ACTIONS = {
-  ADD_TODO: 'add-todo'
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 }
 
 function reducer(todos, action) {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => {
+        if (todo.id !== action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+    default:
+      return todos
   }
 }
 
@@ -32,6 +51,9 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input type='text' value={name} onChange={e => setName(e.target.value)} />
       </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
     </>
   );
 }
